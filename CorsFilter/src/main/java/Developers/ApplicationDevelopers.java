9d42f2c19ddeb8e87bd81066cc8de4f61edd3f28
gvaldes <gvaldes@novutek.com>
2017-01-12 17:58:57 -0700

    Configuracion Spring Security

--- CorsFilter/src/main/java/Developers/ApplicationDevelopers.java
+++ CorsFilter/src/main/java/Developers/ApplicationDevelopers.java
@@ -1,6 +1,5 @@
 package Developers;
 
-import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
